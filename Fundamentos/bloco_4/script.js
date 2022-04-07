6c31df29901fb611bfabcaeaa1c4b243b1217f22
Vinicius Eduardo <vinicius.eddu99@gmail.com>
2022-04-07 15:58:59 -0300

    feito primeiros exercicios

---
+++ Fundamentos/bloco_4/script.js
@@ -0,0 +1,9 @@
+const a = 55;
+const b = 65;
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
---
+++ Fundamentos/bloco_4/script2.js
@@ -0,0 +1,44 @@
+/*
+
+let age = 18;
+let number2 = 230;
+
+
+/*
+
+
+if (condicao) {
+    //faz alguma coisa
+    console.log('number1 é menor que number2');
+} else {
+    //senão faça essa outra coisa
+    console.log('number1 não é menor que number2')
+}
+
+*/
+/*
+if (age > 18) {
+    //faz alguma coisa
+    console.log('Pessoa pode dirigir');
+} else if (age < 18) {
+    console.log('Pessoa não pode dirigir');
+} else {
+    console.log('Corre tirar a carteira!');
+}
+
+
+//Para switch tem que ser colocado TODOS os valores.
+
+/*
+
+switch (teste) {
+    valor1: asdasdasdasd
+    valor2: asdasdasd
+}
+
+*/
+
+
+//0ff - Aula com noel acima.
+
+
