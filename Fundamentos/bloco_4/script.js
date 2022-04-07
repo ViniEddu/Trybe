4e5557bcbe3e3470b492ce5cea384f4ca3837dc0
Vinicius Eduardo <vinicius.eddu99@gmail.com>
2022-04-07 20:02:42 -0300

    começando exercicio 10

--- Fundamentos/bloco_4/script.js
+++ Fundamentos/bloco_4/script.js
@@ -71,3 +71,48 @@ switch (peca) {
     default:
         console.log('Mensagem de erro')
 }
+
+/* exercicio 7 */
+
+let porcentagem = 100;
+
+if (porcentagem >= 90 && porcentagem <= 100) {
+    console.log('A');
+} else if (porcentagem >= 80 && porcentagem <= 100) {
+    console.log('B');
+} else if (porcentagem >= 70 && porcentagem <= 100) {
+    console.log('C');
+} else if (porcentagem >= 60 && porcentagem <= 100) {
+    console.log('D');
+} else if (porcentagem >= 50 && porcentagem <= 100) {
+    console.log('E');
+} else if (porcentagem < 50 && porcentagem > 0) {
+    console.log('F');
+} else if (porcentagem < 0 || porcentagem > 100) {
+    console.log('ou tu é muito zica, ou tu é mt burro!')
+}
+
+
+/* exercicio 8 */
+
+const nume1 = 22;
+const nume2 = 3;
+const nume3 = 35;
+
+if (nume1 % 2 === 0 || nume2 % 2 === 0 || nume3 % 2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+/* exercicio 9 */
+
+const numer1 = 15;
+const numer2 = 3;
+const numer3 = 35;
+
+if (numer1 % 2 === 1 || numer2 % 2 === 1 || numer3 % 2 === 1) {
+    console.log(true);
+} else {
+    console.log(false);
+}
