75de6a99564ca81c6090b912bb2cdb0eedd0ceb7
Vinicius Eduardo <vinicius.eddu99@gmail.com>
2022-04-08 22:51:05 -0300

    palavra invertida

--- Fundamentos/bloco_4/script.js
+++ Fundamentos/bloco_4/script.js
@@ -75,3 +75,21 @@ for (let index = 10; index > 0; index -= 1) {
 }
 
 console.log(fatorial)
+
+//Invertendo palavra
+
+let palavra = 'Jéssica de paula Hermes';
+
+console.log('Ultima letra da palavra está ocupando o espaço: ',palavra.length -1)
+console.log('A letra que ocupa esse espaço é: ', palavra[6])
+
+ let novaPalavra = [];
+
+for (let index = palavra.length - 1; index >= 0; index -= 1) {
+    novaPalavra += palavra[index];
+}
+
+console.log('A palavra é: ',palavra)
+console.log('A palavra invertida fica: ', novaPalavra);
+
+//
