ac0981a07e82c404f90c5b404d55d8d4134fb484
Vinicius Eduardo <vinicius.eddu99@gmail.com>
2022-06-13 21:16:15 -0300

    testes

---
+++ Front_End/React/testando-meu-computador/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const textJSX = 'JSX';
+    const element = <h1>Hello, {textJSX}</h1>
+    return element
+}
+
+export default App;
